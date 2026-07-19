0e692dd2ae2cd29e69bbaa313aae51f45cd25903
Giã Dương Đức Minh <giaduongducminh@gmail.com>
2026-07-19 14:28:49 +0700

    Implement guessing game in a separate module

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,5 @@
-use std::cmp::Ordering;
-use std::io;
+mod ch02;
 
 fn main() {
-    println!("Guess the number!");
-
-    let secret_number = rand::random_range(1..=100);
-    println!("Secret number is {secret_number}");
-
-    loop {
-        println!("Please input your guess.");
-
-        let mut guess = String::new();
-
-        io::stdin()
-            .read_line(&mut guess)
-            .expect("Failed to read line");
-
-        let guess: u32 = match guess.trim().parse() {
-            Ok(num) => num,
-            Err(_) => continue,
-        };
-
-        println!("You guessed: {guess}");
-
-        match guess.cmp(&secret_number) {
-            Ordering::Less => println!("Too small!"),
-            Ordering::Greater => println!("Too big!"),
-            Ordering::Equal => {
-                println!("You win!");
-                break;
-            }
-        }
-    }
+    ch02::guessing_game();
 }
